76cb147bae80cf9250a4212b7cde84a435c04730
Alparslan Korkmaz <korkmazalparslan.elt@gmail.com>
2022-12-28 13:58:21 +0300

    client-side form valid & defined expressErr class

--- seeds/index.js
+++ seeds/index.js
@@ -12,14 +12,14 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for(let i = 0; i < 50; i++) {
+    for(let i = 0; i < 5; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() *20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
-            description: 'Description will be here.',
+            image: `https://source.unsplash.com/random/300x300?camping,${i}`,
+            description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta sunt sint officia non porro distinctio quam, provident aperiam debitis suscipit vero praesentium, quas veritatis tenetur officiis magni aspernatur iusto asperiores.',
             price
         })
         await camp.save(); 
